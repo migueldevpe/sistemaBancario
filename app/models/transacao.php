2aa0360516c3bc73fad4ff741df883ebeabbec38
Miguel <miguelsoaresabs7@gmail.com>
2026-08-06 16:29:19 -0300

    refactor(transacao): pequenas mudanças no código

--- app/models/transacao.php
+++ app/models/transacao.php
@@ -1,28 +1,16 @@
-<!-- tipo
-valor
-data
-contaOrigem
-contaDestino -->
-
 <?php 
   class Transacao {
     private string $tipo;
     private float $valor;
     private DateTime $data;
-    private ?string $contaOrigem;
-    private ?string $contaDestino;
 
     public function __construct(
       string $tipo,
       float $valor,
-      ?string $contaOrigem,
-      ?string $contaDestino
     ) {
       $this->tipo = $tipo;
       $this->valor = $valor;
       $this->data = new DateTime();
-      $this->contaOrigem = $contaOrigem;
-      $this->contaDestino = $contaDestino;
     }
 
     public function getResumo(): string {
